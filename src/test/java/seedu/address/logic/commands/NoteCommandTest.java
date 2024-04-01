b23bd0441b89500d3d3ec2840904f62af17819a6
lihaoquan <lihaoquan.dev@gmail.com>
2024-04-01 20:11:53 +0800

    Fix test fails

--- src/test/java/seedu/address/logic/commands/NoteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/NoteCommandTest.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalApplicants.getTypicalApplicantsAddressBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,23 +11,26 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.applicant.Applicant;
 import seedu.address.model.person.Note;
 import seedu.address.model.person.Person;
+import seedu.address.testutil.ApplicantBuilder;
 import seedu.address.testutil.PersonBuilder;
 
 class NoteCommandTest {
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model model = new ModelManager(getTypicalApplicantsAddressBook(), new UserPrefs());
 
     @Test
     void execute_addNoteUnfilteredList_success() {
 
-        Person targetPerson = model.getFilteredPersonList().get(0);
-        Person editedPerson = new PersonBuilder(targetPerson).withNote("Updated Note").build();
+        Applicant targetPerson = (Applicant) model.getFilteredPersonList().get(0);
+        Applicant editedPerson = (Applicant) new ApplicantBuilder(targetPerson).withNote("Updated Note").build();
 
         NoteCommand executeCommand = new NoteCommand(INDEX_FIRST_PERSON, new Note("Updated Note"), false);
 
-        String expectedResult = String.format(executeCommand.MESSAGE_ADD_NOTE_SUCCESS, Messages.format(editedPerson));
+        String expectedResult = String.format(executeCommand.MESSAGE_ADD_NOTE_SUCCESS, Messages.formatNoteTest(editedPerson));
+        System.out.println(expectedResult);
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         expectedModel.setPerson(targetPerson, editedPerson);
@@ -38,12 +41,12 @@ class NoteCommandTest {
     @Test
     void execute_addNoteUnfilteredListWithDate_success() {
 
-        Person targetPerson = model.getFilteredPersonList().get(0);
-        Person editedPerson = new PersonBuilder(targetPerson).withNote("Updated Note").withNoteDate().build();
+        Applicant targetPerson = (Applicant) model.getFilteredPersonList().get(0);
+        Applicant editedPerson = (Applicant) new ApplicantBuilder(targetPerson).withNote("Updated Note").withNoteDate().build();
 
         NoteCommand executeCommand = new NoteCommand(INDEX_FIRST_PERSON, new Note("Updated Note"), true);
 
-        String expectedResult = String.format(executeCommand.MESSAGE_ADD_NOTE_SUCCESS, Messages.format(editedPerson));
+        String expectedResult = String.format(executeCommand.MESSAGE_ADD_NOTE_SUCCESS, Messages.formatNoteTest(editedPerson));
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         expectedModel.setPerson(targetPerson, editedPerson);
